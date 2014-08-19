eaf049b2cd3f1e39f7db4bf4bcba6616b7ef17aa
ronaldo-trindade <ronaldo.trindade.projetos@gmai.com>
2014-08-19 00:14:20 -0300

    Update x10_receptor.ino
    Modificação para usar apenas vetores e não utilizar strings. Falta testar no arduino.

--- x10_receptor.ino
+++ x10_receptor.ino
@@ -1,5 +1,5 @@
-boolean vetor[18], lido=0, flag=0;
-byte leitura= B00000000, inicio= B00001110, mascara=00001111;
+boolean vetor[18], inicio[4] = {1,1,1,0},lido=0, flag=0, a=0, b=0, c=0, d=0;
+
 int cont=0;
 
 void setup(){
@@ -11,8 +11,15 @@ void loop(){
 }
 
 void burst(){
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  // If interrupts come faster than 200ms, assume it's a bounce and ignore
+  if (interrupt_time - last_interrupt_time > 200){
    lido = digitalRead(10);
-   leitura << lido;
+   a=b;
+   b=c;
+   c=d;
+   d=lido;
    
    if(flag){
      vetor[cont] = lido;
@@ -20,19 +27,20 @@ void burst(){
    
    cont++;
    
-   if ((leitura & mascara) == inicio){
+   if (((inicio[0] == a) and (inicio[1] == b)) and ((inicio[2] == c) and (inicio[3] == d))){
      cont = 0;
      flag = 1;
    }
    
-  if (cont ==18){
+   if (cont ==18){
     cont=0;
     flag=0;
-    leitura = 0;
     for(int i=0; i<19; i++){
         Serial.print(vetor[i]);
     }
     Serial.println();
+   }
   }
+  last_interrupt_time = interrupt_time; 
    
 }
