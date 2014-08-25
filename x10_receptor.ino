e5bb9490a891239b53f35d4d98264618bff45db8
ronaldo-trindade <ronaldo.trindade.projetos@gmai.com>
2014-08-25 02:56:36 +0000

    Update x10_receptor.ino
    receptor funcionando sen string

--- x10_receptor.ino
+++ x10_receptor.ino
@@ -1,52 +1,59 @@
-boolean  lido=0, flag=0;
-String leitura= "", inicio= "1110", palavra="";
+boolean vetor[18], inicio[4] = {1,1,1,0},lido=0, flag=0, a=0, b=0, c=0, d=0;
+
 int cont=0;
 
 void setup(){
-  attachInterrupt(0, burst, CHANGE);
-  Serial.begin(9600);
+  attachInterrupt(0, burst, FALLING);
   pinMode(10,INPUT);
+  Serial.begin(9600);
 }
 
 void loop(){
 }
 
 void burst(){
-  static unsigned long last_interrupt_time = 0;
-  unsigned long interrupt_time = millis();
+//  static unsigned long last_interrupt_time = 0;
+//  unsigned long interrupt_time = millis();
   // If interrupts come faster than 200ms, assume it's a bounce and ignore
-  if (interrupt_time - last_interrupt_time > 200)  {
-    delayMicroseconds(500);// pegar no meio do bit que foi recebido
-    lido = digitalRead(10);
-    Serial.println(lido);
-    if(lido) leitura += "1";
-    else leitura += "0";
-    if (leitura.length() > 4){
-      leitura = leitura.substring(leitura.length()-4);
-    }
-    Serial.print("Leitura:");
-    Serial.println(leitura);
-    if(flag){
-      if(lido) palavra += "1";
-      else palavra += "0";
-    }
+//  if (interrupt_time - last_interrupt_time > 200){
+    delayMicroseconds(200);
+   lido = digitalRead(10);
+//   Serial.print(lido);
+   a=b;
+   b=c;
+   c=d;
+   d=lido;
    
-    cont++;
-    if (leitura == inicio){
-      cont = 0;
-      flag = 1;
-      Serial.println("Achou!");
+   if(flag){
+     vetor[cont] = lido;
+   }
+   
+   cont++;
+   
+   if (((inicio[0] == a) and (inicio[1] == b)) and ((inicio[2] == c) and (inicio[3] == d))){
+     cont = 0;
+     flag = 1;
+   }
+   
+   if ((cont ==18) && flag){
+    cont=0;
+    flag=0;
+    detachInterrupt(0);
+    Serial.println(); 
+    Serial.println("House:");
+    for(int i=0; i<8; i++){
+        Serial.print(vetor[i]);
     }
-    
-    if (cont ==18){
-      cont=0;
-      flag=0;
-      leitura = 0;
-      Serial.println();
-      Serial.print("Recebido:");
-      Serial.print(palavra);
-      Serial.println();
+    Serial.println();
+    Serial.println("Codigo:");
+    for(int i=0; i<10; i++){
+        Serial.print(vetor[i+8]);
     }
-  }
-  last_interrupt_time = interrupt_time; 
+    Serial.println();
+    attachInterrupt(0, burst, FALLING);
+   }
+//  }
+//  last_interrupt_time = interrupt_time; 
+   
 }
+
